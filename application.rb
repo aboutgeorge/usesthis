96249d3561a949b164f4ce7e6bbef685811a105a
Daniel Bogan <waferbaby@superfreak.local>
2009-05-25 20:24:09 +1000

    Initial import.

---
+++ application.rb
@@ -0,0 +1,22 @@
+#
+#    this is usesthis.com, a sinatra application.
+#    it is copyright (c) 2009 daniel bogan (d @ waferbaby, then a dot and a 'com')
+#
+
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+require 'sass'
+
+get '/' do
+    haml :index
+end
+
+get %r{/about/?} do
+    haml :about
+end
+
+get %r{/stylesheets/(screen|mobile)/?} do |filename|
+    content_type 'text/css', :charset => 'utf-8'
+    sass filename
+end
