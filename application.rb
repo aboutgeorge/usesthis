253f11142bff39b4401b369d76dbf7d1d907ab7b
Daniel Bogan <github.com@waferbaby.com>
2009-06-01 14:51:59 +1000

    Added the DM setup, and the includes for the models. Updated the index method to actually do stuff.

--- application.rb
+++ application.rb
@@ -5,11 +5,34 @@
 
 require 'rubygems'
 require 'sinatra'
+require 'datamapper'
 require 'haml'
 require 'sass'
 
+configure do
+    
+    begin
+        config = YAML.load_file('application.yml')
+
+        DataMapper.setup(:default, config[:datamapper])
+    	DataMapper::Logger.new(STDOUT, :debug)
+    	
+    	Dir.glob('lib/*.rb') do |filename|
+    	    require filename
+	    end
+    	
+    rescue Exception => e
+        puts "Error: #{e}"
+        exit
+    end
+end
+
 get '/' do
+    @interviews = Interview.all
     haml :index
+end
+
+get %r{/(.+)/?} do
 end
 
 get %r{/about/?} do
