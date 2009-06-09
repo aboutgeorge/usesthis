fd8f8041db0f239749e23f4b44609043be543e48
Daniel Bogan <github.com@waferbaby.com>
2009-06-09 23:00:08 +1000

    A refactoring as a sub-class of Sinatra::Base, with a file-based publishing system.

--- usesthis.rb
+++ usesthis.rb
@@ -4,82 +4,110 @@
 #
 
 require 'rubygems'
-require 'sinatra'
+require 'sinatra/base'
 require 'datamapper'
 require 'haml'
 require 'sass'
-require 'rdiscount'
 
-configure do |p|    
-    begin
-        config = YAML.load_file('usesthis.yml')
+module Setup
+    class Interview
+        attr_accessor :person, :slug, :contents, :date
         
-        DataMapper.setup(:default, config[:database][p.environment])
-    	
-        Dir.glob('lib/*.rb') do |filename|
-            require filename
-	    end
-	    
-    rescue Exception => e
-        puts "Error: #{e}"
-        exit
-    end
-end
+        def self.all
+            interviews = []
 
-helpers do    
-    def markup(text, filter_html = true)
-        return "" unless text
+            Dir.glob(File.join("interviews", "published", "*.markdown")) do |path|
+                interviews << Setup::Interview.new(path)
+            end
+
+            interviews
+        end
         
-        markdown = RDiscount.new(text)
-        markdown.filter_html = filter_html
+        def self.with_slug(slug)
+            files = Dir.glob(File.join("interviews", "published", "*#{slug}.markdown"))
+            return nil unless files.length == 1
+
+            Setup::Interview.new(files[0])
+        end
         
-        markdown.to_html
+        def initialize(path)
+            matches = %r(/(\d{4,})-(\d{2,})-(\d{2,})-(.+)\.markdown$).match(path)
+            
+            @person     = matches[4].split(".").collect{ |word| word.capitalize }.join(" ")
+            @slug       = matches[4].downcase
+            @contents   = File.read(path)
+            @date       = {:year => matches[1], :month => matches[2], :day => matches[3]}
+        end
     end
     
-    def content_for(interview)
-        content  = "### Who are you and what do you do?\n"
-        content += "#{interview.introduction}\n\n"
-        content += "### What hardware do you use?\n"
-        content += "#{interview.hardware_used}\n\n"
-        content += "### And what software?\n"
-        content += "#{interview.software_used}\n\n"
-        content += "### What would be your dream setup?\n"
-        content += "#{interview.dream_setup}"
-
-        if interview.wares.length > 0
-            content += "\n\n"
-
-            interview.wares.each do |ware|
-                content += "[#{ware.slug}]: #{ware.url} \"#{ware.description}\"\n"
+    class Publisher
+        def self.run
+            wares = YAML.load_file(File.join("data", "wares.yml"))
+            
+            questions = [
+                "Who are you and what do you do?",
+                "What hardware do you use?",
+                "And what software?",
+                "What would be your dream setup?"
+            ]
+            
+            Dir.glob(File.join("interviews", "drafts", "*.markdown")) do |path|
+                time = Time.now
+                contents = File.read(path)
+                
+                unless contents.blank?
+                    contents.gsub!(/### Question (\d):/) do |matches|
+                        "### #{questions[$1.to_i-1]}"
+                    end
+                    
+                    contents += "\n\n"
+                    
+                    links = contents.scan(/\[([^\[\(\)]+)\]\[([a-z0-9\.\-]+)?\]/)
+                    links.each do |link|
+                        slug = link[1].blank? ? link[0].downcase : link[1]
+                        
+                        if wares[slug]
+                            contents += "[#{wares[slug]['title']}](#{wares[slug]['url']} \"#{wares[slug]['description']}\")\n"
+                        end
+                    end
+                    
+                    new_path = File.join("interviews", "published", "#{time.strftime('%Y')}-#{time.strftime('%m')}-#{time.strftime('%d')}-#{File.basename(path)}")
+                    File.open(new_path, "w+") do |f|
+                        f.write(contents)
+                    end
+                    
+                    File.unlink(path)
+                end
             end
         end
-
-        content
     end
-end
-
-get '/' do
-    @interviews = Interview.all(:order => [:created_at.desc])
-    haml :index
-end
-
-get '/about/?' do
-end
-
-get '/feed/?' do
-end
-
-get '/:slug/?' do |slug|
-    @interview = Interview.first(:slug => slug)
-    raise not_found unless @interview
     
-    haml :interview
-end
+    class App < Sinatra::Base
+        get '/' do
+            @interviews = Interview.all
+            haml :index
+        end
 
-get '/stylesheets/:filename/?' do |filename|
-    content_type 'text/css', :charset => 'utf-8'
-    sass filename.to_sym
-end
+        get '/about/?' do
+            haml :about
+        end
 
-not_found do
+        get '/feed/?' do
+        end
+
+        get '/:slug/?' do |slug|
+            @interview = Interview.with_slug(slug)
+            raise not_found unless @interview
+            
+            haml :interview
+        end
+
+        get '/stylesheets/:filename/?' do |filename|
+            content_type 'text/css', :charset => 'utf-8'
+            sass filename.to_sym
+        end
+
+        not_found do
+        end
+    end
 end
