632f98fb7cf43a33b22c79095873f343717357f6
Daniel Bogan <github.com@waferbaby.com>
2009-06-13 12:31:10 +1000

    Ditched the Publisher class in favour of a rake task.

--- application.rb
+++ application.rb
@@ -44,49 +44,6 @@ module Setup
         end
     end
     
-    class Publisher
-        def self.run
-            wares = YAML.load_file(File.join("data", "wares.yml"))
-
-            questions = [
-                "Who are you and what do you do?",
-                "What hardware do you use?",
-                "And what software?",
-                "What would be your dream setup?"
-            ]
-            
-            Dir.glob(File.join("interviews", "drafts", "*.markdown")) do |path|
-                contents = File.read(path)
-                
-                unless contents.empty?
-                    contents.sub!(/^(.+)\r\n\r\n/, '\1' + " @ #{DateTime.now.to_s}\r\n\r\n")
-                    contents.gsub!(/### Question (\d)/) do |match|
-                        "### #{questions[$1.to_i-1]}"
-                    end
-                    
-                    links = contents.scan(/\[([^\[\(\)]+)\]\[([a-z0-9\.\-]+)?\]/)
-                    if links.length > 0
-                        contents += "\n\n"
-                        
-                        links.each do |link|
-                            slug = link[1] ? link[1] : link[0].downcase                        
-                            if wares["#{slug}"]
-                                contents += "[#{slug}]: #{wares[slug]['url']} \"#{wares[slug]['description']}\"\n"
-                            end
-                        end
-                    end
-                    
-                    new_path = File.join("interviews", "published", File.basename(path))
-                    File.open(new_path, "w+") do |f|
-                        f.write(contents)
-                    end 
-                    
-                    File.unlink(path)
-                end
-            end
-        end
-    end
-    
     class App < Sinatra::Base
         set :haml, { :format => :html5 }
         
