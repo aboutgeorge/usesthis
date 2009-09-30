794b616e6039412c92825c45474387d18fe5c66a
Daniel Bogan <github.com@waferbaby.com>
2009-09-30 21:41:48 +1000

    Changed back to a classic Sinatra app, split off the admin.

--- usesthis.rb
+++ usesthis.rb
@@ -3,126 +3,49 @@
 #    it is copyright (c) 2009 daniel bogan (d @ waferbaby, then a dot and a 'com')
 #
 
-require 'rubygems'
-require 'sinatra/base'
-require 'datamapper'
-require 'rdiscount'
-require 'haml'
-require 'sass'
+%w[rubygems sinatra datamapper haml lib/interview lib/ware].each do |lib|
+    require lib
+end
 
-require 'lib/interview'
-require 'lib/ware'
+set :haml, {:format => :html5}
 
-module UsesThis
-    class Base < Sinatra::Base
-        set :haml, {:format => :html5}
-    
-        configure do
-            begin
-                @database = YAML.load_file('config/database.yml')
-            rescue
-                puts "Error: Unable to read database config."
-                exit
-            end
-    
-            DataMapper.setup(:default, @database)
-        end
-    
-        helpers do
-            def current_page
-                @page = params[:page] && params[:page].match(/\d+/) ? params[:page].to_i : 1
-            end
-        end
+configure do
+    DataMapper.setup(:default, YAML.load_file(File.join(File.dirname(__FILE__), 'config', 'database.yml')))
+end
+
+helpers do
+    def current_page
+        @page = params[:page] && params[:page].match(/\d+/) ? params[:page].to_i : 1
     end
+end
 
-    class Main < Base
-        get '/' do
-            @count, @interviews = Interview.paginated(:is_public => true, :page => current_page, :per_page => 5, :order => [:created_at.desc])
-            haml :index
-        end
+get '/' do
+    @count, @interviews = Interview.paginated(:is_public => true, :page => current_page, :per_page => 5, :order => [:created_at.desc])
+    haml :index
+end
 
-        get '/about/?' do
-            haml :about
-        end
+get '/about/?' do
+    haml :about
+end
 
-        get '/feed/?' do
-            content_type 'application/atom+xml', :charset => 'utf-8'
+get '/feed/?' do
+    content_type 'application/atom+xml', :charset => 'utf-8'
 
-            @interviews = Interview.all(:order => [:created_at.desc])
-            haml :feed, {:format => :xhtml, :layout => false}
-        end
+    @interviews = Interview.all(:order => [:created_at.desc])
+    haml :feed, {:format => :xhtml, :layout => false}
+end
 
-        get '/:slug/?' do
-            @interview = Interview.first(:slug => params[:slug])
-            raise not_found unless @interview
+get '/:slug/?' do
+    @interview = Interview.first(:slug => params[:slug])
+    raise not_found unless @interview
 
-            haml :interview
-        end
+    haml :interview
+end
 
-        not_found do
-            haml :error, :locals => { :message => "Man, I suck. I can't find what you're looking for." }
-        end
+not_found do
+    haml :error, :locals => { :message => "Man, I suck. I can't find what you're looking for." }
+end
 
-        error do
-            haml :error, :locals => { :message => "Something bad happened. It's not you, it's me. Probably." }
-        end
-    end
-
-    class Admin < Base
-        configure do
-            begin
-                @config = YAML.load_file('config/admin.yml')
-            rescue
-                puts "Error: Unable to read admin config."
-                exit
-            end
-        end
-    
-        use Rack::Auth::Basic do |username, password|
-            username == @config[:name] && OpenSSL::Digest::SHA1.new(password).hexdigest == @config[:password]
-        end
-    
-        get '/wares/?' do
-            @wares = Ware.all(:order => [:slug.asc])
-            haml :'wares/index'
-        end
-
-        get '/interviews/?' do
-            @count, @interviews = Interview.paginated(:page => current_page, :per_page => 10, :order => [:created_at.desc])
-            haml :'interviews/index'
-        end
-
-        get '/interviews/new' do
-            @interview = Interview.new
-            haml :'interviews/form', :locals => {:path => '/interviews/new'}
-        end
-
-        post '/interviews/new' do
-            @interview = Interview.new(params)
-
-            if @interview.save
-                redirect '/interviews'
-            else
-                haml :'interviews/form', :locals => {:path => '/interviews/new'}
-            end
-        end
-
-        get '/interviews/:slug' do
-            @interview = Interview.first(:slug => params[:slug])
-            raise not_found unless @interview
-
-            haml :'interviews/form', :locals => {:path => "/interviews/#{params[:slug]}"}
-        end
-
-        post '/interviews/:slug' do
-            @interview = Interview.first(:slug => params[:slug])
-            raise not_found unless @interview
-
-            if @interview.update(params)
-                redirect '/interviews'
-            else
-                haml :'interviews/form', :locals => {:path => "/admin/#{params[:slug]}"}
-            end
-        end
-    end
+error do
+    haml :error, :locals => { :message => "Something bad happened. It's not you, it's me. Probably." }
 end
