774d58137625111570b60f4f71bba1896762f76c
Daniel Bogan <github.com@waferbaby.com>
2009-10-02 13:29:28 +1000

    Increased the URL length.

--- lib/ware.rb
+++ lib/ware.rb
@@ -13,7 +13,7 @@ class Ware
     
     property :slug,         String, :key => true
     property :title,        String
-    property :url,          String, :length => 100
+    property :url,          String, :length => 250
     property :description,  String, :length => 100
     
     timestamps :at
