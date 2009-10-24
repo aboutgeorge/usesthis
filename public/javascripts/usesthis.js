3d106e226f759d462eae2b9e14a305da956ab707
Daniel Bogan <github.com@waferbaby.com>
2009-10-25 00:53:54 +1100

    Only add methods if we have a slug.

--- public/javascripts/usesthis.js
+++ public/javascripts/usesthis.js
@@ -1,43 +1,45 @@
 $(document).ready(function(){
-	var edit_url = '/' + slug + '/edit/';
+	if (typeof(slug) != 'undefined'){
+		var edit_url = '/' + slug + '/edit/';
 	
-	$('a#publish').click(function(){
-		if(!window.confirm('Publish this interview?')) {
-			return false;
-		}
+		$('a#publish').click(function(){
+			if(!window.confirm('Publish this interview?')) {
+				return false;
+			}
 		
-		$.post(edit_url + 'published_at', { published_at: Date() }, function(result){
-			$('p.unpublished').replaceWith("<time>" + result + "</time>");
+			$.post(edit_url + 'published_at', { published_at: Date() }, function(result){
+				$('p.unpublished').replaceWith("<time>" + result + "</time>");
+			});
+		
+			return true;
 		});
-		
-		return true;
-	});
 	
-	$('h2.person').editable(edit_url + 'person', {
-		cssclass: 'editor',
-		name: 'person',
-		id: '',
-	});
+		$('h2.person').editable(edit_url + 'person', {
+			cssclass: 'editor',
+			name: 'person',
+			id: '',
+		});
 	
-	$('img.portrait + p').editable(edit_url + 'credits', {
-		cssclass: 'editor',
-		name: 'credits',
-		id: '',
-		loadurl: '/' + slug + '/credits',
-	});
+		$('img.portrait + p').editable(edit_url + 'credits', {
+			cssclass: 'editor',
+			name: 'credits',
+			id: '',
+			loadurl: '/' + slug + '/credits',
+		});
 	
-	$('p.summary').editable(edit_url + 'summary', {
-		cssclass: 'editor',
-		name: 'summary',
-		id: '',
-	});
+		$('p.summary').editable(edit_url + 'summary', {
+			cssclass: 'editor',
+			name: 'summary',
+			id: '',
+		});
 	
-	$('article.contents').editable(edit_url + 'contents', {
-		cssclass: 'editor',
-		name: 'contents',
-		id: '',
-		loadurl: '/' + slug + '/contents',
-		type: 'textarea',
-		submit: 'OK',
-	});	
+		$('article.contents').editable(edit_url + 'contents', {
+			cssclass: 'editor',
+			name: 'contents',
+			id: '',
+			loadurl: '/' + slug + '/contents',
+			type: 'textarea',
+			submit: 'OK',
+		});
+	}
 });
